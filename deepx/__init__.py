4cedc688f6194dca36846795088371bcc82aca31
kYangLi <lyang.1915@gmail.com>
2026-01-08 08:56:26 +0800

    feat: prepare for the online docs

---
+++ deepx/__init__.py
@@ -0,0 +1,27 @@
+print("\033[1;32mTo use the DeepH JAX version (DeepX), please apply for usage permission at:\033[0m")
+print("\033[4;34m  - https://ticket.deeph-pack.com\033[0m\n")
+print("\033[1;32mIf you use `DeepH-pack` in your work, please cite the following publications:\033[0m\n")
+print("\033[1;33mThe original framework paper introducing the foundational methodology:\033[0m")
+print("    He Li, Zun Wang, Nianlong Zou, et al. Deep-learning density functional theory Hamiltonian for efficient ab initio electronic-structure calculation. Nat. Comput. Sci. 2, 367 (2022)")
+print("\033[4;34m    - https://doi.org/10.1038/s43588-022-00265-6\033[0m\n")
+print("\033[1;33mComplete package featuring the latest implementation, methodology, and workflow:\033[0m")
+print("    Yang Li, Yanzhen Wang, Boheng Zhao, et al. DeepH-pack: A general-purpose neural network package for deep-learning electronic structure calculations. arXiv:2601.02938 (2026)")
+print("\033[4;34m    - https://arxiv.org/abs/2601.02938\033[0m\n")
+print("\033[1;32mBibTeX entries:\033[0m\n")
+print('''@article{li2022deep,
+    title={Deep-learning density functional theory Hamiltonian for efficient ab initio electronic-structure calculation},
+    author={Li, He and Wang, Zun and Zou, Nianlong and Ye, Meng and Xu, Runzhang and Gong, Xiaoxun and Duan, Wenhui and Xu, Yong},
+    journal={Nat. Comput. Sci.},
+    volume={2},
+    number={6},
+    pages={367},
+    year={2022},
+    publisher={Nature Publishing Group US New York}
+}
+
+@article{li2026deeph,
+    title={DeepH-pack: A general-purpose neural network package for deep-learning electronic structure calculations},
+    author={Li, Yang and Wang, Yanzhen and Zhao, Boheng and Gong, Xiaoxun and Wang, Yuxiang and Tang, Zechen and Wang, Zixu and Yuan, Zilong and Li, Jialin and Sun, Minghui and Chen, Zezhou and Tao, Honggeng and Wu, Baochun and Yu, Yuhang and Li, He and da Jornada, Felipe H. and Duan, Wenhui and Xu, Yong },
+    journal={arXiv preprint arXiv:2601.02938},
+    year={2026}
+}''')
